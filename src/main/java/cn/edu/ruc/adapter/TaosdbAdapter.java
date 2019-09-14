de55f6b60c791c598795c1107391a8fc75500a11
taoyu <1205022533@qq.com>
2019-09-15 00:48:19 +0800

    add the taosdb adapter

---
+++ src/main/java/cn/edu/ruc/adapter/TaosdbAdapter.java
@@ -0,0 +1,16 @@
+package cn.edu.ruc.adapter;/**
+ * @program: tsbm
+ * @description: ${description}
+ * @author: rainmaple
+ * @date: 2019-09-14 22:40
+ **/
+
+/**
+ * @ClassName TaosdbAdapter
+ * @Description: TODO
+ * @Author rainmaple
+ * @Date 2019/9/14 
+ * @Version V1.0
+ **/
+public class TaosdbAdapter {
+}
